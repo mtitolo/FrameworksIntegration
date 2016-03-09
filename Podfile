325938431ad25b0e03b9f648504856d95fb27c7e
Titolo, Michele <github@michele.io>
2016-03-09 13:34:22 -0800

    Switch to Frameworks

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '9.0'
 
+use_frameworks!
+
 target 'FrameworksIntegration' do 
 	pod 'CocoaLumberjack'
 end
--- Pods/Target Support Files/Pods-FrameworksIntegration/Pods-FrameworksIntegration-frameworks.sh
+++ Pods/Target Support Files/Pods-FrameworksIntegration/Pods-FrameworksIntegration-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$CONFIGURATION_BUILD_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$CONFIGURATION_BUILD_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+fi
